14a379ef90f597a4f7f22506b25809ef0796117c
manu <victorx811@gmail.com>
2025-09-13 19:51:27 -0300

    Installed vite-plugin-federation as dev dependencie and configurated vite.config.ts to call federation plugin remotes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// host-app/vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "host-app",
+      remotes: {
+        welcome: "http://localhost:5002/assets/remoteEntry.js",
+        clients: "http://localhost:5001/assets/remoteEntry.js",
+        selected: "http://localhost:5003/assets/remoteEntry.js",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  server: {
+    port: 5000,
+  },
+});
